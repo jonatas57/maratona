47a8870825d8d7ec71eef9ef86fe35c57b34f719
jonatas57 <jdjonatasduarte57@gmail.com>
2019-02-13 17:22:10 -0200

    error: seg fault in string reading

--- ahoy.cpp
+++ ahoy.cpp
@@ -19,9 +19,10 @@ typedef pair<int, int>     ii;
 #define left(x)            x << 1
 #define right(x)           x << 1 | 1
 
-vi st;
-vector<ii> lazy;
-int size;
+#define T                  1000
+#define SIZE               4 * T + 10
+
+int st[SIZE] = {0}, lazy[SIZE];
 
 void build(string& v, int id, int l, int r) {
 	if (l == r) {
@@ -35,32 +36,35 @@ void build(string& v, int id, int l, int r) {
 	}
 }
 
+void propaux(int& x, int y) {
+	if (y == 0 || y == 1) x = y;
+	else if (y == -1) {
+		if (x == INF) x = y;
+		else if (x == -1) x = INF;
+		else x ^= 1;
+	}
+}
 void prop(int id, int l, int r) {
-	int x, y;
-	tie(x, y) = lazy[id];
-	if (y >= 0) {
-		if (x == 1) st[id] = r - l + 1;
-		else if (x == 0) st[id] = 0;
-		else {
-			st[id] = r - l + 1 - st[id];
-		}
+	if (lazy[id] <= 1) {
+		if (lazy[id] == 1)      st[id] = r - l + 1;
+		else if (lazy[id] == 0) st[id] = 0;
+		else                    st[id] = r - l + 1 - st[id];
+
 		if (l != r) {
-			if (lazy[left(id)].second != -1) prop(left(id), l, avg(l, r));
-			if (lazy[right(id)].second!= -1) prop(right(id), avg(l, r) + 1, r);
-			lazy[left(id)] = lazy[id];
-			lazy[right(id)] = lazy[id];
+			propaux(lazy[left(id)], lazy[id]);
+			propaux(lazy[right(id)], lazy[id]);
 		}
-		lazy[id] = {0, -1};
+		lazy[id] = INF;
 	}
 }
 /*@*/
-// void propAll(int id, int l, int r) {
-// 	prop(id, l, r);
-// 	if (l == r) return;
-// 	int mid = avg(l, r);
-// 	propAll(left(id), l, mid);
-// 	propAll(right(id), mid + 1, r);
-// }
+void propAll(int id, int l, int r) {
+	prop(id, l, r);
+	if (l == r) return;
+	int mid = avg(l, r);
+	propAll(left(id), l, mid);
+	propAll(right(id), mid + 1, r);
+}
 /*@*/
 int query(int id, int a, int b, int l, int r) {
 	prop(id, l, r);
@@ -74,7 +78,7 @@ void update(int id, int a, int b, int l, int r, int val) {
 	prop(id, l, r);
 	if (a > r or b < l) return;
 	if (l >= a and r <= b) {
-		lazy[id] = {val, 0};
+		lazy[id] = val;
 		prop(id, l, r);
 	}
 	else {
@@ -85,65 +89,111 @@ void update(int id, int a, int b, int l, int r, int val) {
 	}
 }
 /*@*/
-// string buffer;
-// void printBase(int id, int l, int r ) {
-// 	if (l == r) {
-// 		buffer[l] = st[id] + '0';
-// 	}
-// 	else {
-// 		int mid = avg(l, r);
-// 		printBase(left(id), l, mid);
-// 		printBase(right(id), mid + 1, r);
-// 	}
-// }
-// void print() {
-// 	buffer.clear();
-// 	buffer.resize(size, '-');
-// 	propAll(1, 0, size - 1);
-// 	printBase(1, 0, size - 1);
-// 	cout << buffer << endl;
-// }
+string buffer;
+void printBase(int id, int l, int r ) {
+	if (l == r) {
+		buffer[l] = st[id] + '0';
+	}
+	else {
+		int mid = avg(l, r);
+		printBase(left(id), l, mid);
+		printBase(right(id), mid + 1, r);
+	}
+}
+void print() {
+	buffer.clear();
+	buffer.resize(SIZE, '-');
+	propAll(1, 0, SIZE - 1);
+	printBase(1, 0, SIZE - 1);
+	cout << buffer << endl;
+}
 /*@*/
-int main() {
-	ios_base::sync_with_stdio(false);
 
-	int t, cs = 1;
-	cin >> t;
-	loop(t) {
-		string s = "", y;
+struct pirates {
+	vector<char*> pir;
+	vector<int> x;
+	int qtd;
+
+	pirates() : qtd(0) {x.pb(0);}
+	void insert(int n, char* s) {
+		cout << __LINE__ << endl;
+		pir.pb(s);
+		cout << __LINE__ << endl;
+		x.pb(x[++qtd] + n * sizeof(s) / sizeof(char));
+		cout << __LINE__ << endl;
+	}
+	int getChar(int i) {
+		cout << __LINE__ << endl;
+		int j = binSearch(1, qtd, i);
+		cout << j << endl;
+		cout << __LINE__ << endl;
+		int aux = j;
+		cout << __LINE__ << endl;
+		return pir[j][aux] - '0';
+	}
+	int binSearch(int l, int r, int val) {
+		cout << __LINE__ << endl;
+		if (l == r) return l;
+		cout << __LINE__ << endl;
+		int mid = avg(l, r);
+		cout << __LINE__ << endl;
+		if (val >= x[mid - 1] && val < x[mid]) {
+			cout << __LINE__ << endl;
+			return mid;
+		}
+		else if (val < x[mid - 1]) {
+			cout << __LINE__ << endl;
+			return binSearch(l, mid, val);
+		}
+		else return binSearch(mid + 1, r, val);
+	}
+};
+
+int main() {
+	pirates p;
+	// int t, cs = 1;
+	// scanf("%d", &t);
+	// loop(t) {
+		// string s = "";
+		char *y;
 		int m;
-		cin >> m;
+		scanf("%d\n", &m);
 		loop(m) {
 			int q;
-			cin >> q >> y;
-			loop(q) {
-				s += y;
-			}
-		}
-		size = s.size();
-		st.clear(), st.resize(4 * size + 10);
-		lazy.clear(), lazy.resize(4 * size + 10, {0, -1});
-		build(s, 1, 0, size - 1);
-
-		int q, a, b;
-		char c;
-		cin >> q;
-		cout << "Case " << cs++ << ":" << endl;
-		int qu = 1;
-		loop(q) {
-			cin >> c >> a >> b;
-			if (c == 'S') {
-				cout << "Q" << qu++ << ": ";
-				cout << query(1, a, b, 0, size - 1) << endl;
-			}
-			// else if (c == 'P') {
-			// 	propAll(1, 0, size - 1);
-			// 	print();
+			scanf("%d\n", &q);
+			scanf("%s\n", &y);
+			// loop(q) {
+			// 	s += string(y);
 			// }
-			else {
-				update(1, a, b, 0, size - 1, (c == 'F' ? 1 : (c == 'E' ? 0 : -1)));
-			}
+			p.insert(q, y);
 		}
-	}
+		while(cin >> m) {
+			cout << p.getChar(m) << endl;
+		}
+	// 	int size = s.size();
+	// 	memset(st, 0, sizeof(st));
+	// 	memset(lazy, INF, sizeof(lazy));
+	// 	build(s, 1, 0, size - 1);
+	//
+	// 	int q, a, b;
+	// 	char c;
+	// 	scanf("%d", &q);
+	// 	printf("Case %d:\n", cs++);
+	// 	int qu = 1;
+	// 	loop(q) {
+	// 		scanf("\n%c %d %d", &c, &a, &b);
+	// 		if (c == 'S') {
+	// 			printf("Q%d: ", qu++);
+	// 			printf("%d\n", query(1, a, b, 0, size - 1));
+	// 		}/*@*/
+	// 		else if (c == 'P') {
+	// 			propAll(1, 0, SIZE - 1);
+	// 			print();
+	// 		}/*@*/
+	// 		else {
+	// 			update(1, a, b, 0, size - 1, (c == 'F' ? 1 : (c == 'E' ? 0 : -1)));
+	// 		}
+	// 	}
+	// }
 	return 0;
 }
