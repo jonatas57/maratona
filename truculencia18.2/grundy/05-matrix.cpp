8edaefa69d1ab04ac37338e84b89c80d45b23f8c
= <=>
2019-04-15 14:30:11 -0300

    grundy ex 4-5

---
+++ truculencia18.2/grundy/05-matrix.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef vector<int>	       vi;
+typedef vector<bool>       vb;
+typedef map<int, int>      mii;
+typedef pair<int, int>     ii;
+
+#define INF                0x3f3f3f3f
+#define MAX                100000
+#define N                  4 * MAX + 10
+#define MOD                1000000007
+#define each(x, s)         for(auto& x : s)
+#define ceach(x, s)        for(auto const& x : s)
+#define rep(x, y, z)       for(int i = x;i < y;i+=z)
+#define loop(x)	           for(int i = 0;i < x;i++)
+#define vloop(v, x)        for(int v = 0;v < x;v++)
+#define avg(l, r)          l + (r - l) / 2
+
+int main() {
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, m;
+		cin >> n >> m;
+		vi v(m);
+		int ans = 0;
+		loop(n) {
+			loop(m) {
+				cin >> v[i];
+			}
+			int gn = 0;
+			for (int i = m - 1;i >= 0;i--) {
+				gn = v[i] - (v[i] <= gn ? 1 : 0);
+			}
+			ans ^= gn;
+		}
+		cout << (ans ? "FIRST\n" : "SECOND\n");
+	}
+	return 0;
+}
+
