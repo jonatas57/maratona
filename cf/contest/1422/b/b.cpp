1e38af33b787a76c596ad826d4c65c140b4142d4
jonatas57 <jdjonatasduarte57@gmail.com>
2020-10-04 23:19:27 -0300

    675 div2

---
+++ cf/contest/1422/b/b.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef vector<int>         vi;
+typedef vector<bool>       vb;
+typedef map<int, int>      mii;
+typedef pair<int, int>     ii;
+
+#define INF                0x3f3f3f3f
+#define INFLL              0x3f3f3f3f3f3f3f3f
+#define each(x, s)         for(auto& x : s)
+#define loop(x)            for(int i = 0;i < x;i++)
+#define vloop(v, x)        for(int v = 0;v < x;v++)
+#define avg(l, r)          l + (r - l) / 2
+#define iter(a)            a.begin(), a.end()
+#define riter(a)           a.rbegin(), a.rend()
+#define endl               "\n"
+
+const ll mod = 1000000007;
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+  int t;
+  cin >> t;
+  loop(t) {
+    int n, m;
+    cin >> n >> m;
+    vector<vi> a(n, vi(m));
+    loop(n) vloop(j, m) cin >> a[i][j];
+    ll ans = 0;
+    for (int il = 0, ir = n - 1;il <= ir;il++, ir--) {
+      for (int jl = 0, jr = m - 1;jl <= jr;jl++, jr--) {
+        vi aux = {a[il][jl], a[ir][jr]};
+        if (il != ir) aux.push_back(a[ir][jl]);
+        if (jl != jr) aux.push_back(a[il][jr]);
+        int med = aux.size() / 2;
+        sort(iter(aux));
+        each(x, aux) ans += abs(x - aux[med]);
+      }
+    }
+    cout << ans << endl;
+  }
+  return 0;
+}
+
