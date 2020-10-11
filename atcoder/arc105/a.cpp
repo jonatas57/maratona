1b7aa604a4d6537f865962d4d4329509a1a6a0aa
jonatas57 <jdjonatasduarte57@gmail.com>
2020-10-11 17:59:17 -0300

    atcoder arc105

---
+++ atcoder/arc105/a.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef vector<int>         vi;
+typedef vector<bool>       vb;
+typedef map<int, int>      mii;
+typedef pair<int, int>     ii;
+
+#define INF                0x3f3f3f3f
+#define INFLL              0x3f3f3f3f3f3f3f3f
+#define each(x, s)         for(auto& x : s)
+#define loop(x)            for(int i = 0;i < x;i++)
+#define vloop(v, x)        for(int v = 0;v < x;v++)
+#define avg(l, r)          l + (r - l) / 2
+#define iter(a)            a.begin(), a.end()
+#define riter(a)           a.rbegin(), a.rend()
+#define endl               "\n"
+
+const ll mod = 1000000007;
+
+bool solve(vi& a, int n, int s) {
+  if (a[n - 1] > s or (n < 0 and s)) return false;
+  if (a[n - 1] == s) return true;
+  return solve(a, n - 1, s - a[n - 1]) | solve(a, n - 1, s);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+  int sum = 0;
+  vi a(4);
+  loop(4) {
+    cin >> a[i];
+    sum += a[i];
+  }
+  bool ans = false;
+  if (~sum & 1) {
+    sum /= 2;
+    ans = solve(a, 4, sum);
+  }
+  cout << (ans ? "Yes" : "No") << endl;
+  return 0;
+}
+
