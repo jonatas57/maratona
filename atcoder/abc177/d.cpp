3cc6f9dc1adcf4321fb7a68ba50fc1543ff7247d
jonatas57 <jdjonatasduarte57@gmail.com>
2020-10-26 13:05:32 -0300

    atcoder abc177

---
+++ atcoder/abc177/d.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef vector<int>         vi;
+typedef vector<bool>       vb;
+typedef map<int, int>      mii;
+typedef pair<int, int>     ii;
+
+#define INF                0x3f3f3f3f
+#define INFLL              0x3f3f3f3f3f3f3f3f
+#define each(x, s)         for(auto& x : s)
+#define loop(x)            for(int i = 0;i < x;i++)
+#define vloop(v, x)        for(int v = 0;v < x;v++)
+#define avg(l, r)          l + (r - l) / 2
+#define iter(a)            a.begin(), a.end()
+#define riter(a)           a.rbegin(), a.rend()
+#define endl               "\n"
+
+const ll mod = 1000000007;
+
+vi id, sz;
+void init(int x) {
+  id.resize(x);
+  sz.resize(x, 1);
+  loop(x) id[i] = i;
+}
+int find(int x) {
+  return x == id[x] ? x : id[x] = find(id[x]);
+}
+void join(int a, int b) {
+  a = find(a);
+  b = find(b);
+  if (a == b) return;
+  if (sz[a] > sz[b]) swap(a, b);
+  id[a] = id[b];
+  sz[b] += sz[a];
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+  int n, m;
+  cin >> n >> m;
+  int a, b;
+  init(n);
+  loop(m) {
+    cin >> a >> b;
+    join(a - 1, b - 1);
+  }
+  int ans = 0;
+  loop(n) ans = max(ans, sz[find(i)]);
+  cout << ans << endl;
+  return 0;
+}
+
---
+++ atcoder/abc177/e.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef vector<int>         vi;
+typedef vector<bool>       vb;
+typedef map<int, int>      mii;
+typedef pair<int, int>     ii;
+
+#define INF                0x3f3f3f3f
+#define INFLL              0x3f3f3f3f3f3f3f3f
+#define each(x, s)         for(auto& x : s)
+#define loop(x)            for(int i = 0;i < x;i++)
+#define vloop(v, x)        for(int v = 0;v < x;v++)
+#define avg(l, r)          l + (r - l) / 2
+#define iter(a)            a.begin(), a.end()
+#define riter(a)           a.rbegin(), a.rend()
+#define endl               "\n"
+
+const ll mod = 1000000007;
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+  int n;
+  cin >> n;
+  mii tfs;
+  int x, g = 0;
+  bool pw = true;
+  loop(n) {
+    cin >> x;
+    g = gcd(g, x);
+    if (!pw) continue;
+    mii fs;
+    for (int i = 2;i * i <= x;i++) {
+      while (x % i == 0) {
+        fs[i]++;
+        x /= i;
+      }
+    }
+    if (x > 1) fs[x]++;
+    for (auto& [b, e] : fs) {
+      if (tfs[b]) pw = false;
+      else tfs[b] += e;
+    }
+  }
+  cout << (pw ? "pairwise coprime" : g == 1 ? "setwise coprime" : "not coprime") << endl;
+  return 0;
+}
+
