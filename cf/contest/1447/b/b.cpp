27ec0e839550fddd4e94a4b7373e6db0360ab97a
jonatas57 <jdjonatasduarte57@gmail.com>
2020-11-15 15:42:36 -0300

    683 div2

---
+++ cf/contest/1447/b/b.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef vector<int>         vi;
+typedef vector<bool>       vb;
+typedef map<int, int>      mii;
+typedef pair<int, int>     ii;
+
+#define INF                0x3f3f3f3f
+#define INFLL              0x3f3f3f3f3f3f3f3f
+#define each(x, s)         for(auto& x : s)
+#define loop(x)            for(int i = 0;i < x;i++)
+#define vloop(v, x)        for(int v = 0;v < x;v++)
+#define avg(l, r)          l + (r - l) / 2
+#define iter(a)            a.begin(), a.end()
+#define riter(a)           a.rbegin(), a.rend()
+#define endl               "\n"
+
+const ll mod = 1000000007;
+const double EPS = 1e-7;
+const double PI = acos(-1);
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+  int t;
+  cin >> t;
+  loop(t) {
+    int n, m;
+    cin >> n >> m;
+    ll sum = 0, x, y = INF, neg = 0;
+    bool rem = true;
+    loop(n) vloop(j, m) {
+      cin >> x;
+      sum += abs(x);
+      if (x) y = min(y, abs(x));
+      else rem = false;
+      if (x < 0) neg++;
+    }
+    if (rem and neg & 1) sum -= y + y;
+    cout << sum << endl;
+  }
+  return 0;
+}
