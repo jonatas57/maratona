d62abb6bb5c24b6bb54f2b6b01d2bad9b0d1a35e
jonatas57 <jdjonatasduarte57@gmail.com>
2021-06-25 15:56:20 -0300

    728 div2

---
+++ cf/contest/1541/b/b.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef vector<int>         vi;
+typedef vector<bool>       vb;
+typedef map<int, int>      mii;
+typedef pair<int, int>     ii;
+
+#define INF                0x3f3f3f3f
+#define INFLL              0x3f3f3f3f3f3f3f3f
+#define each(x, s)         for(auto& x : s)
+#define loop(x)            for(int i = 0;i < x;i++)
+#define vloop(v, x)        for(int v = 0;v < x;v++)
+#define avg(l, r)          (l + r) >> 1
+#define iter(a)            a.begin(), a.end()
+#define riter(a)           a.rbegin(), a.rend()
+#define endl               "\n"
+
+const ll mod = 1000000007;
+const double EPS = 1e-7;
+const double PI = acos(-1);
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+  int t;
+  cin >> t;
+  loop(t) {
+    int n;
+    cin >> n;
+    vi a(n + 1);
+    loop(n) cin >> a[i + 1];
+    ll ans = 0;
+    for (int j = 2;j <= n;j++) {
+      for (int ai = (j + j - 1) / a[j];ai * a[j] - j > 0;ai--) {
+        if (a[ai * a[j] - j] == ai) ans++;
+      }
+    }
+    cout << ans << endl;
+  }
+  return 0;
+}
+
---
+++ cf/contest/1541/c/c.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef vector<int>         vi;
+typedef vector<bool>       vb;
+typedef map<int, int>      mii;
+typedef pair<int, int>     ii;
+
+#define INF                0x3f3f3f3f
+#define INFLL              0x3f3f3f3f3f3f3f3f
+#define each(x, s)         for(auto& x : s)
+#define loop(x)            for(int i = 0;i < x;i++)
+#define vloop(v, x)        for(int v = 0;v < x;v++)
+#define avg(l, r)          (l + r) >> 1
+#define iter(a)            a.begin(), a.end()
+#define riter(a)           a.rbegin(), a.rend()
+#define endl               "\n"
+
+const ll mod = 1000000007;
+const double EPS = 1e-7;
+const double PI = acos(-1);
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+  int t;
+  cin >> t;
+  loop(t) {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    loop(n) cin >> a[i];
+    ll ans = 0, acc = 0;
+    sort(iter(a));
+    for (int i = 1;i < n;i++) {
+      ll e = a[i] - a[i - 1];
+      ans += e;
+      acc += i * e;
+      ans -= acc;
+    }
+    cout << ans << endl;
+  }
+  return 0;
+}
+
